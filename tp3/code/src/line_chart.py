cae00251166e2aa19b7754a8541885dad4650196
Saga Palmér <sagapa@kth.se>
2023-02-25 17:17:21 -0500

    fixed dates and hover template

--- tp3/code/src/line_chart.py
+++ tp3/code/src/line_chart.py
@@ -94,4 +94,5 @@ def get_figure(line_data:pd.DataFrame, arrond:str, year:int):
     fig = px.line(line_data,x='Date_Plantation', y= 'Trees',title=title)
     fig.update_layout(xaxis_title = "")
     fig.update(data=[{'hovertemplate': hover_template.get_linechart_hover_template()}])
+    fig.update_xaxes(tickformat="%d %b")
     return fig
